6cd54c703a2f505141363079c6a06c8eb7db409d
ayshafaris99 <aaj427@nyu.edu>
2021-04-05 18:25:11 -0400

    week10

--- 2021/week10/theMedium.js
+++ 2021/week10/theMedium.js
@@ -1,5 +1,21 @@
 let goal = document.getElementById("theCause"); 
 let box1 = document.getElementById("animate1"); 
 let box2 = document.getElementById("animate2");
-let theButton = document.getElementById("myButton");
-theButton.addEventListener('click', myanimation);
+let theButton = document.getElementById("button");
+theButton.addEventListener('click', myanimation());
+
+let id = null;
+function myanimation() {  
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      box1.style.top = pos + "px"; 
+      box1.style.left = pos + "px"; 
+    }
+  }
+}
